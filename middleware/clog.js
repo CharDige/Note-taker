8b6603a7205efa89542961c3262ccb9babe34cdf
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-05-24 18:26:32 +0930

    Added custom middleware to input coloured logs when requests are made

---
+++ middleware/clog.js
@@ -0,0 +1,22 @@
+// Customer middleware which logs the type and path of each request to the server
+const clog = (req, res, next) => {
+    const fgGreen = "\x1b[32m";
+    const fgCyan = "\x1b[36m";
+    const fgYellow = "\x1b[33m";
+    switch (req.method) {
+        case 'GET': {
+            console.info(`${fgGreen}${req.method} request to ${req.path}`);
+            break;
+        }
+        case 'POST': {
+            console.info(`${fgCyan}${req.method} request to ${req.path}`);
+            break;
+        }
+        default:
+            console.log(`${fgYellow}${req.method} request to ${req.path}`);
+    }
+
+    next();
+};
+
+exports.clog = clog;
