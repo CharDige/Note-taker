87a02b60a99c98f7d321da0437b7607fb7083d9d
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-05-24 16:17:12 +0930

    Added in module requirements, middleware and GET route for homepage

---
+++ server.js
@@ -0,0 +1,25 @@
+// Module requirements
+const express = require('express');
+const path = require('path');
+
+// PORT
+const PORT = 3001;
+
+// app variable with Express.js
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+// GET route for homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+// App listening to PORT
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`)
+});
