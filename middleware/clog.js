6a18468d0da73a8ce72f2e0b32370fe9920997be
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-05-25 18:27:36 +0930

    Updated cLog so that DELETE requests are in a different colour when logged

--- middleware/clog.js
+++ middleware/clog.js
@@ -3,6 +3,7 @@ const clog = (req, res, next) => {
     const fgGreen = "\x1b[32m";
     const fgCyan = "\x1b[36m";
     const fgYellow = "\x1b[33m";
+    const fgRed = "\x1b[31m"
     switch (req.method) {
         case 'GET': {
             console.info(`${fgGreen}${req.method} request to ${req.path}`);
@@ -10,6 +11,10 @@ const clog = (req, res, next) => {
         }
         case 'POST': {
             console.info(`${fgCyan}${req.method} request to ${req.path}`);
+            break;
+        }
+        case 'DELETE': {
+            console.info(`${fgRed}${req.method} request to ${req.path}`);
             break;
         }
         default:
