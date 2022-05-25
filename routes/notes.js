b5924aecdf5d497399a36cb93d34f05b01f0d58c
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-05-25 15:38:15 +0930

    Added POST route for adding a new note

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,7 @@
 // Using Express Router
 const notes = require('express').Router();
 
+const e = require('express');
 // Module exports
 const { v4: uuidv4 } = require('uuid');
 const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
@@ -10,4 +11,24 @@ notes.get('/', (req, res) => {
     readFromFile('./db/db.json')
     .then((data) =>
     res.json(JSON.parse(data)));
+});
+
+// POST route for adding a new note
+notes.post('/', (req, res) => {
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json('Note added!');
+    } else {
+        res.error('Error - Note not added');
+    }
 });
